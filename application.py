00854d3a2a0597483802f8b81370def14e2b4be3
shreyasarjun <shreyas.arjun007@gmail.com>
2022-04-27 20:02:48 +0530

    small change2

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route('/', methods=['GET','POST'])
 def hello_world():
-    return 'Hello World 5678'
+    return 'Hello World Shreyas'
 
 
 if __name__ == '__main__':
